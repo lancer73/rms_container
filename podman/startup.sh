efcf5869925ea5124ccfead4bfbf8b1f7e0390c8
lancer73 <33584546+lancer73@users.noreply.github.com>
2024-03-09 11:38:01 +0100

    Create startup.sh
    Initial commit

---
+++ podman/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp /RMS_data/config/.config /source/RMS
+cp -pr /RMS_data/config/.ssh /
+cp /RMS_data/config/mask* /source/RMS
+cp /RMS_data/config/platepar* /source/RMS
+chmod 500 .ssh/*
+chmod 700 .ssh
+source /vRMS/bin/activate
+cd /source/RMS
+/Desktop/RMS_FirstRun.sh
