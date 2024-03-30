daf43b2cdd9436ab26bb800b8cc8b56b28558ce0
lancer73 <33584546+lancer73@users.noreply.github.com>
2024-03-30 12:33:45 +0100

    Update startup.sh
    Files in .ssh were read only preventing known_hosts from being written

--- podman/startup.sh
+++ podman/startup.sh
@@ -3,7 +3,7 @@ cp /RMS_data/config/.config /source/RMS
 cp -pr /RMS_data/config/.ssh /
 cp /RMS_data/config/mask* /source/RMS
 cp /RMS_data/config/platepar* /source/RMS
-chmod 500 .ssh/*
+chmod 600 .ssh/*
 chmod 700 .ssh
 source /vRMS/bin/activate
 cd /source/RMS
